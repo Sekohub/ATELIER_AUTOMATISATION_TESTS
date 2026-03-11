69312502bf07a22d559e58ec777e845a37c6760b
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 16:58:57 +0100

    Update client.py

--- Tester/client.py
+++ Tester/client.py
@@ -7,13 +7,25 @@ def call_api():
 
     start = time.time()
 
-    r = requests.get(API_URL, timeout=3)
+    try:
+        r = requests.get(API_URL, timeout=3)
 
-    latency = time.time() - start
+        latency = time.time() - start
 
-    return {
-        "status": r.status_code,
-        "latency": latency,
-        "json": r.json(),
-        "headers": r.headers
-    }
+        return {
+            "status": r.status_code,
+            "latency": latency,
+            "json": r.json(),
+            "headers": r.headers
+        }
+
+    except requests.exceptions.RequestException:
+
+        latency = time.time() - start
+
+        return {
+            "status": 503,
+            "latency": latency,
+            "json": {},
+            "headers": {}
+        }
