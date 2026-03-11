050cc54671ede93c96b3cbb51742f349dad212f0
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 15:07:06 +0100

    Update tests.py

--- Tester/tests.py
+++ Tester/tests.py
@@ -1,1 +1,23 @@
+def test_status(response):
+    return response["status"] == 200
 
+
+def test_radar_exists(response):
+
+    data = response["json"]
+
+    return "radar" in data
+
+
+def test_past_frames(response):
+
+    radar = response["json"]["radar"]
+
+    return "past" in radar and len(radar["past"]) > 0
+
+
+def test_time_field(response):
+
+    frame = response["json"]["radar"]["past"][0]
+
+    return "time" in frame
