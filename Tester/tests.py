3bf6769cc83bdab6514d9b2d9db258a89735da69
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 15:19:35 +0100

    Update tests.py

--- Tester/tests.py
+++ Tester/tests.py
@@ -2,7 +2,12 @@ def test_status(response):
     return response["status"] == 200
 
 
-def test_radar_exists(response):
+def test_content_type(response):
+
+    return "application/json" in response["headers"]["Content-Type"]
+
+
+def test_radar_field(response):
 
     data = response["json"]
 
@@ -11,13 +16,11 @@ def test_radar_exists(response):
 
 def test_past_frames(response):
 
-    radar = response["json"]["radar"]
-
-    return "past" in radar and len(radar["past"]) > 0
+    return "past" in response["json"]["radar"]
 
 
-def test_time_field(response):
+def test_time_type(response):
 
     frame = response["json"]["radar"]["past"][0]
 
-    return "time" in frame
+    return isinstance(frame["time"], int)
