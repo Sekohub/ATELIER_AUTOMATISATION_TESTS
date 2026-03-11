0280661242eb33619f341399cbb80d3c8ec7790b
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 15:08:10 +0100

    Update runner.py

--- Tester/runner.py
+++ Tester/runner.py
@@ -1,1 +1,17 @@
+from tester.client import call_api
+from tester.tests import *
 
+def run():
+
+    response = call_api()
+
+    results = {
+        "status": response["status"],
+        "latency": response["latency"],
+        "test_status": test_status(response),
+        "test_radar": test_radar_exists(response),
+        "test_frames": test_past_frames(response),
+        "test_time": test_time_field(response)
+    }
+
+    return results
