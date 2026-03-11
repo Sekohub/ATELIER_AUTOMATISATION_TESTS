eef543f5890d426780e35e56271b21d995dbb234
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 15:23:38 +0100

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+DB = "runs.db"
+
+def save_run(status, latency):
+
+    conn = sqlite3.connect(DB)
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS runs(
+        id INTEGER PRIMARY KEY,
+        status INTEGER,
+        latency REAL
+    )
+    """)
+
+    conn.execute(
+        "INSERT INTO runs(status,latency) VALUES (?,?)",
+        (status, latency)
+    )
+
+    conn.commit()
