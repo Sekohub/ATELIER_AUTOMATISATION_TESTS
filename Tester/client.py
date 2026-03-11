3fe13060130578c83d1b8afe2bc030eabf87ae2f
SerkanHub <ozerserkan05@gmail.com>
2026-03-11 15:04:07 +0100

    Update client.py

--- Tester/client.py
+++ Tester/client.py
@@ -1,1 +1,18 @@
+import requests
+import time
 
+API_URL = "https://api.rainviewer.com/public/weather-maps.json"
+
+def call_api():
+
+    start = time.time()
+
+    r = requests.get(API_URL, timeout=5)
+
+    latency = time.time() - start
+
+    return {
+        "status": r.status_code,
+        "latency": latency,
+        "json": r.json()
+    }
